2c217647fa8ce3232b5a09f78d98e6ae76e0f52d
Lilin Lao <lll9p.china@gmail.com>
2020-10-28 11:54:28 +0800

    make edge important

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ from .Nested_UNet import NestedUNet
 from .UNet_3Plus import UNet_3Plus, UNet_3Plus_DeepSup, UNet_3Plus_DeepSup_CGM
 __all__ = ['UNet',
            "UNet_2Plus",
-           'NestedUnet',
+           'NestedUNet',
            "UNet_3Plus",
            "UNet_3Plus_DeepSup",
            "UNet_3Plus_DeepSup_CGM"
