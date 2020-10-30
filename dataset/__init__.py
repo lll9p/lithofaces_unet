a9d0fd27fedbbd6b3d170c2abca5d3f578ab6028
Lilin Lao <lll9p.china@gmail.com>
2020-10-30 14:57:13 +0800

    add preprocess

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,3 +1,8 @@
 from .dataset import Dataset
 from .dataset import get_datasets
 from .preprocess_lithofaces import dataset_split_256
+__all__ = [
+    'Dataset',
+    'get_datasets',
+    'dataset_split_256'
+]
