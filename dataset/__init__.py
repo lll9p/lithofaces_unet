096d0eadcef1cc24d6aa6d23c6eb0256bc842287
Lilin Lao <lll9p.china@gmail.com>
2020-11-01 22:31:10 +0800

    update

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,9 +1,9 @@
 from .dataset import Dataset, get_datasets
-from .preprocess_lithofaces import dataset_split_256, prepare_dataset_224
+from .preprocess_lithofaces import dataset_perform, prepare_dataset_224
 
 __all__ = [
     'Dataset',
     'get_datasets',
-    'dataset_split_256',
+    'dataset_perform',
     'prepare_dataset_224'
 ]
