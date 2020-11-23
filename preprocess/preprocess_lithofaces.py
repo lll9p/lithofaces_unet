8e658f87b8257fd797122e1cee875aeaeda2b774
Lilin Lao <lll9p.china@gmail.com>
2020-11-23 17:26:35 +0800

    update

--- preprocess/preprocess_lithofaces.py
+++ preprocess/preprocess_lithofaces.py
@@ -267,7 +267,7 @@ def split_to_256(image, mask, label):
             mask_resized[edges] = 0
             mask_new, touched = fix_edge(mask_resized)
             mask_new[edges] = label['edges'][0]
-            mask_new[touched.astype(bool)] = label['edges'][0]
+            #mask_new[touched.astype(bool)] = label['edges'][0]
         blocks = list(split(new_shape, 256))
         for block in blocks:
             pad_flag, [y, y_stop], [x, x_stop] = block
