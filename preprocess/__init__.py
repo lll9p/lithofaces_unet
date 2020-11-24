b8bb22dc9f211553b498e7ec053db304bd8cabad
Lilin Lao <lll9p.china@gmail.com>
2020-11-24 11:25:29 +0800

    update

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -1,8 +1,9 @@
-from .preprocess_lithofaces import process_original, class_weight, dataset_to_h5, form_datasets
+from .preprocess_lithofaces import process_original, class_weight, dataset_to_h5, form_datasets, get_val_images
 
 __all__ = [
     'process_original',
     'class_weight',
     'dataset_to_h5',
-    'form_datasets'
+    'form_datasets',
+    'get_val_images'
 ]
